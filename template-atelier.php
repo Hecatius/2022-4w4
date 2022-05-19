3edd2b20f1da040fd76695783aae03feb0855a8c
Jonathan Mores <jonathanmopro@gmail.com>
2022-05-19 14:47:25 -0400

    petit css du template atelier

--- template-atelier.php
+++ template-atelier.php
@@ -1,7 +1,7 @@
 <?php /*Template Name: Atelier*/ ?>
 
 <?php get_header() ?>
-<main class="site__main">
+<main class="site__main atelier">
     <!-- <h1>----template-evenement----</h1> -->
     <?php if (have_posts()): while(have_posts()): the_post(); ?>
     <?php the_post_thumbnail()?>
