5a2d60acf510ac9b7a10a166f054f4af8c360df4
Jonathan Mores <jonathanmopro@gmail.com>
2022-05-19 14:34:47 -0400

    ajout des composant du template

--- template-atelier.php
+++ template-atelier.php
@@ -7,15 +7,14 @@
     <?php the_post_thumbnail()?>
     <h1><?php the_title() ?></h1>
     <br>
-    <p class="atelier__description"><?php the_field('description_de_l’atelier') ?></p>
-    <p class="atelier__animateur"><?php the_field('animateur') ?></p>
-    <p><?php the_field('date_de_debut_de_l’atelier'); ?></p>
-    <p><?php the_field('date_de_fin_de_l’atelier'); ?></p>
-    <p><?php the_field('jours_de_la_semaine_de_l’atelier'); ?></p>
-    <p><?php the_field('heure_de_debut'); ?></p>
-    <p><?php the_field('heure_de_fin'); ?></p>
-    <p><?php the_field('duree_d’une_seance_d’atelier'); ?></p>
-    <p><?php the_field('local_ou_se_deroulera_l’atelier'); ?></p>
+    <p class="atelier__description"> Description de l'atelier :  <?php the_field('description_de_l’atelier') ?></p>
+    <p class="atelier__animateur">l'animateur de l'atelier :  <?php the_field('animateur') ?></p>
+    <p>Date de début :  <?php the_field('date_de_debut_de_l’atelier'); ?></p>
+    <p>Date de fin :  <?php the_field('date_de_fin_de_l’atelier'); ?></p>
+    <p>La formation se donnera :  <?php the_field('jours_de_la_semaine_de_l’atelier'); ?></p>
+    <p>L'horraire de :  <?php the_field('heure_de_debut'); ?> à <?php the_field('heure_de_fin'); ?></p>
+    <p>Durée de chacune des séances est de <?php the_field('duree_d’une_seance_d’atelier'); ?> heures</p>
+    <p>L'atelier sera donné au local :  <?php the_field('local_ou_se_deroulera_l’atelier'); ?></p>
     <?php endwhile ?>
     <?php endif ?>
 </main>
